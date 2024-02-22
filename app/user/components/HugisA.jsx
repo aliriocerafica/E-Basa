b3140e4c23a8f723bcebb9488e85cc13766c77d7
aliriocerafica <ceraficaalejandrojr@gmail.com>
2024-02-22 21:33:21 +0800

    Hugis A Hugis B page

---
+++ app/user/components/HugisA.jsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+import "../css/quiz.css";
+const handleChoiceClick = (choice) => {
+  console.log("Clicked on:", choice);
+};
+
+const QuizPage1 = () => {
+  return (
+    <div className="page-container flex flex-wrap">
+        <div>
+      <button className=" text-white font-bold py-2 px-4 rounded responsive ">
+        <img src="/back2.png" alt="Image Alt Text" className="h-[260px]  w-[80px] inline mr-6" />
+    
+      </button>
+      
+    </div>
+    
+      <div className="w-[1400px] h-[700px] p-2 bg-[#20683C] border-8 border-[#BF977B] rounded-lg">
+      <div className="Q-card mt-[80px] text-center">
+          <h1 className="tittle text-[40px] text-white">
+          <h1 className="tittle text-[40px] text-[#FF8484]">
+          Panuto: {" "}
+          </h1>
+          Piliin sa mga kahon sa ibaba ang hugis Parisukat.{" "}
+          </h1>
+          <p className="Question text-[25px] text-white">
+            {" "}
+            
+          </p>
+        </div>
+        <div className="Q-card h-20 items-center mb-[50px] flex justify-center ">
+         
+        </div>
+
+        <div className="Choice gap-8 flex flex-wrap justify-center items-center text-white text-shadow-md text-[45px]">
+          <button
+            className="choice-1 shadow-md bg-[#798BFF] h-[240px] w-[240px]  rounded-lg  flex items-center justify-center text-center mb-4 hover:bg-blue-500 hover:text-white"
+            onClick={() => handleChoiceClick("Pagong")}
+          > <img
+          src="/hugis1.png"
+          alt="Image Alt Text"
+          className="h-[190px] w-[160px] "
+        />
+          
+          </button>
+          <button
+            className="choice-2 shadow-md bg-[#49F6FF] rounded-lg h-[240px] w-[240px]  flex items-center justify-center text-center mb-4 hover:bg-blue-500 hover:text-white"
+            onClick={() => handleChoiceClick("Kalapati")}
+          >
+            <img
+          src="/hugis2.png"
+          alt="Image Alt Text"
+          className="h-[190px] w-[160px] "
+        />
+          </button>
+          <button
+            className="choice-3 shadow-md bg-[#35F27F] rounded-lg h-[240px] w-[240px] flex items-center justify-center text-center mb-4 hover:bg-blue-500 hover:text-white"
+            onClick={() => handleChoiceClick("Ibon")}
+          >
+            <img
+          src="/hugis3.png"
+          alt="Image Alt Text"
+          className="h-[190px] w-[160px] "
+        />
+          </button>
+          <button
+            className="choice-4 shadow-md bg-[#0CB0FA] rounded-lg h-[240px] w-[240px]  flex items-center justify-center text-center mb-4 hover:bg-blue-500 hover:text-white"
+            onClick={() => handleChoiceClick("Koneho")}
+          >
+             <img
+          src="/hugis4.png"
+          alt="Image Alt Text"
+          className="h-[170px] w-[160px] "
+        />
+          </button>
+        </div>
+
+       
+      </div>
+      <button className=" text-white font-bold py-2 px-4 rounded responsive ">
+        <img src="/next2.png" alt="Image Alt Text" className="h-[250px] w-[80px] inline ml-6" />
+    
+      </button>
+    </div>
+  );
+};
+
+export default QuizPage1;
--- app/user/quizpage/page.tsx
+++ app/user/quizpage/page.tsx
@@ -1,14 +1,15 @@
 'use clients'
 import React from 'react'
-import QuizPage1 from "../components/QuizPage1";
-import QuizPage2 from "../components/QuizPage2";
+import HugisA from "../components/HugisA";
+import HugisB from "../components/HugisB";
+import QuizPage2 from "../components/HugisB";
 import "../css/quiz.css"; // Corrected import statement
 const page = () => {
   return (
   
     <div className='page-container'>
       
-        <QuizPage1 />
+        <HugisB />
         </div>
   )
 }
