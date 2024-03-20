3d7d6c95b43f03f65d50aec9130d152e0193932b
aliriocerafica <ceraficaalejandrojr@gmail.com>
2024-03-20 23:25:40 +0800

    added changers

--- app/user/(protected)/home/page.tsx
+++ app/user/(protected)/home/page.tsx
@@ -1,47 +1,51 @@
-'use client';
-import React, { useEffect } from 'react';
-import UserNav from '../../components/UserNav'; // Adjust the path as needed
+"use client";
+import React, { useEffect } from "react";
+import UserNav from "../../components/UserNav";
 import "../../css/userpage.css";
+import SideNav from "../../components/SideNav";
 
 export default function Home() {
-  console.log(localStorage.getItem('access_token'));
-  console.log(localStorage.getItem('user_id'));
+  console.log(localStorage.getItem("access_token"));
+  console.log(localStorage.getItem("user_id"));
   return (
     <div className="page-container">
       <UserNav />
+      <SideNav />
+      <div className="prev mr-20">
+        <img src="/prev.png"></img>
+      </div>
       <div className="section1 place-self-center bg-white shadow-md rounded-lg">
-        <h2 className="section-header text-center bg-[#06BEE1] text-[30px] rounded-[5px] text-white shadow-md mb-2 p-1">ARALIN 1</h2>
-        <div className="image-container">
-          <img
-            src="/book1.png" // Replace with the actual path to your image
-            alt="Description of the image"
-            className="section-image"
-          />
-          <button className="image-button">
-            <img
-              src="/simulan.png" // Replace with the actual path to your image
-              alt="Description of the image"
-              className="button-image"
-            />
-          </button>
+        <div className="headerimg bg-[#13DFFE] h-[300px] w-[400px] mb-10 rounded-lg">
+          <img src="/aral.png"></img>
+        </div>
+        <div className="section-content">
+          <h2 className="section-header font-bold text-center text-[30px] rounded-[5px] text-black mb-2 p-1">
+            ARALIN 1
+          </h2>
+          <div className="text-center mt-[100px] ">
+            <button className="text-white w-full text-[20px]  bg-[#13DFFE] p-2 rounded-lg mx-auto shadow-md hover:bg-[#58e9ff] hover:text-black">
+             SIMULAN
+            </button>
+          </div>
         </div>
       </div>
       <div className="section2 bg-white ml-10 shadow-md rounded-lg">
-        <h2 className="section-header text-center bg-[#06BEE1] text-[30px] rounded-[5px] text-white shadow-md mb-2 p-1">GAWAIN 1</h2>
-        <div className="image-container">
-          <img
-            src="/gawain1.png" // Replace with the actual path to your image
-            alt="Description of the image"
-            className="section-image"
-          />
-          <button className="image-button">
-            <img
-              src="/sagutan.png" // Replace with the actual path to your image
-              alt="Description of the image"
-              className="button-image"
-            />
-          </button>
+        <div className="headerimg bg-[#13DFFE] h-[300px] w-[400px] mb-10 rounded-lg">
+          <img src="/Gawa.png"></img>
         </div>
+        <div className="section-content">
+          <h2 className="section-header  font-bold text-center text-[30px] rounded-[5px] text-black mb-2 p-1">
+            GAWAIN 1
+          </h2>
+          <div className="text-center  mt-[100px] ">
+            <button className="text-white w-full text-[20px] bg-[#13DFFE] p-2 rounded-lg mx-auto shadow-md hover:bg-[#58e9ff] hover:text-black">
+              SAGUTAN
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="next ml-20">
+        <img src="/next.png"></img>
       </div>
     </div>
   );
